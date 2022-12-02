0f9b8d35efb647dd5e3109b85e2444541da77d00
Chris A <1drturtle@gmail.com>
2022-12-01 22:04:40 -0500

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,13 @@
+# get input
+with open("day1/input.txt") as f:
+    file_content = f.read()
+    # parse into total calorie count
+    elves = sorted(
+        map(sum, [map(int, elf.splitlines()) for elf in file_content.split("\n\n")]),
+        reverse=True,
+    )
+
+# part one
+print(f"Part 1: Highest # of Calories = {elves[0]}")
+# part two
+print(f"Part 2: Sum of top three = {sum(elves[0:3])}")
