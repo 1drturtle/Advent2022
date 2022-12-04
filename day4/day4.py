7934f0d31525d0bb314917eb1db8cd59b5e8d14b
Chris A <1drturtle@gmail.com>
2022-12-04 00:53:12 -0500

    comment!

--- day4/day4.py
+++ day4/day4.py
@@ -8,13 +8,14 @@ pairs = file_content.splitlines()
 pair_count = 0
 disjoint_count = 0
 
-
+# convert x-y into range(x, y+1)
 def dash_to_range(x: str):
     x = x.split("-")
     return set(range(int(x[0]), int(x[-1]) + 1))
 
 
 for pair in pairs:
+    # unpack pairs
     left, right = (dash_to_range(side) for side in pair.split(","))
     # part one
     if left.issubset(right) or right.issubset(left):
